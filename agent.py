683d1e7d0980cab4444382afe6cfe0b263a24b6e
DomMzzz <domnuthh@gmail.com>
2026-03-11 22:41:41 +0700

    update API

---
+++ agent.py
@@ -0,0 +1,94 @@
+import time
+import requests
+from scapy.all import ARP, Ether, srp
+
+API_URL = "https://clouddom-network-management.onrender.com/api/devices/update"
+API_TOKEN = "my-agent-secret"
+
+NETWORK = "192.168.1.0/24"
+SCAN_INTERVAL = 60
+
+
+def scan_network(network):
+
+    print("Starting network scan:", network)
+
+    arp_request = ARP(pdst=network)
+    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+
+    packet = broadcast / arp_request
+
+    result = srp(packet, timeout=2, verbose=0)[0]
+
+    devices = []
+
+    for sent, received in result:
+
+        device = {
+            "ip": received.psrc,
+            "mac": received.hwsrc,
+            "vendor": "Unknown",
+            "hostname": "N/A",
+            "device_type": "Unknown",
+            "possible_model": "Unknown",
+            "status": "Online"
+        }
+
+        devices.append(device)
+
+    return devices
+
+
+def send_to_cloud(devices):
+
+    headers = {
+        "Content-Type": "application/json",
+        "X-API-KEY": API_TOKEN
+    }
+
+    try:
+
+        response = requests.post(
+            API_URL,
+            json={"devices": devices},
+            headers=headers,
+            timeout=30
+        )
+
+        print("Server response:", response.status_code)
+
+        if response.status_code == 200:
+            print("Upload success")
+        else:
+            print("Upload failed:", response.text)
+
+    except Exception as e:
+
+        print("Error sending to cloud:", str(e))
+
+
+def main():
+
+    while True:
+
+        try:
+
+            print("\nScanning network...")
+
+            devices = scan_network(NETWORK)
+
+            print("Devices found:", len(devices))
+
+            send_to_cloud(devices[:2])
+
+        except Exception as e:
+
+            print("Agent error:", str(e))
+
+        print("Waiting", SCAN_INTERVAL, "seconds...\n")
+
+        time.sleep(SCAN_INTERVAL)
+
+
+if __name__ == "__main__":
+    main()
--- config.py
+++ config.py
@@ -3,7 +3,4 @@ import os
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "super_secret_key_12345")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",
-        "sqlite:///site.db"
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
