462852388aa83455e151a399ef3f579eea5f5e5b
DomMzzz <domnuthh@gmail.com>
2026-03-11 19:56:31 +0700

    upgrade psycopg2-binary for python 3.14

--- config.py
+++ config.py
@@ -2,10 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "super_secret_key_12345")
-
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        "mysql+pymysql://Dom:Dom%401304@127.0.0.1:3307/network_management"
+        "sqlite:///site.db"
     )
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
