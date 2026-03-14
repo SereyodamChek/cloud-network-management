57a77f65ba8dd6452048ef4932f7cccedd323dd9
DomMzzz <domnuthh@gmail.com>
2026-03-14 13:38:52 +0700

    edit config

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "super_secret_key_12345")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "super_secret_key")
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",
-        "mysql+pymysql://Dom:Dom%401304@127.0.0.1:3307/network_management"
-    )
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
